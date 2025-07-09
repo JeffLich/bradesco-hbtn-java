f999a623513e755d577c0c9a6c34d722be9cd12b
Jefferson Mario Lichtenfels <emailparajeff@gmail.com>
2025-07-09 15:15:19 -0300

    subindo metodos

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,40 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.setSaudeAtual(saudeAtual);
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        this.status = saudeAtual > 0 ? "vivo" : "morto";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = (nome == null | nome == "") ? this.nome : nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(Math.max(this.saudeAtual - quantidadeDeDano, 0));
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setSaudeAtual(Math.min(this.saudeAtual + quantidadeDeCura, 100));
+
+    }
+}
