338f5a11c3cc31e6ec85093009473125b3567dd7
Jefferson Mario Lichtenfels <emailparajeff@gmail.com>
2025-07-09 16:34:13 -0300

    subindo metodos

---
+++ Heranca/retangulo/Retangulo.java
@@ -0,0 +1,26 @@
+public class Retangulo extends FormaGeometrica{
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura > 0 )
+            this.largura = largura;
+        else
+            throw new java.lang.UnsupportedOperationException("Largura deve ser maior ou igual a 0");
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura > 0 )
+            this.altura = altura;
+        else
+            throw new java.lang.UnsupportedOperationException("Altura deve ser maior ou igual a 0");
+    }
+}
