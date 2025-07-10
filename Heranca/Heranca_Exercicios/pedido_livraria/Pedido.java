f639395ba967211e4b1e10421dd56578f29a24e4
Jefferson Mario Lichtenfels <emailparajeff@gmail.com>
2025-07-09 22:47:13 -0300

    subindo heranca_exercicios

---
+++ Heranca/Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,18 @@
+public class Pedido {
+    public double percentualDesconto;
+    public ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double calcularTotal(){
+        double total = 0;
+        for (int i = 0; i < itens.length; i++) {
+            total += itens[i].getQuantidade() * itens[i].getProduto().obterPrecoLiquido();
+        }
+        total = percentualDesconto > 0 ? total - (total * (percentualDesconto/100)) : total;
+        return total;
+    }
+}
