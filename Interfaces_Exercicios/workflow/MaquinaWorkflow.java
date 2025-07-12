04aa7cc44aca89b20f17b436e575c1ea66585493
Jefferson Mario Lichtenfels <emailparajeff@gmail.com>
2025-07-12 09:43:33 -0300

    subindo resumo pedido

---
+++ Interfaces_Exercicios/workflow/MaquinaWorkflow.java
@@ -0,0 +1,10 @@
+import atividades.Atividade;
+
+public class MaquinaWorkflow {
+
+    public void executar(Workflow workflow){
+        workflow.getAtividades().forEach(Atividade::executar);
+    }
+
+
+}
